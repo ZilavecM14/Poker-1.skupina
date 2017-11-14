fe2709e19f26c522726893b7344015c4b633a312
Kaja V <kaja.vezensek@gmail.com>
2017-11-14 19:58:21 +0100

    popravek

--- LaRelance.R
+++ LaRelance.R
@@ -5,17 +5,27 @@ LaRelance.opt1 <- function(X, c1) {
   return(X > c1^2)
 }
 
-# optimalna strategija za prvega igralca pri La Relance
+# optimalna strategija za drugega igralca pri La Relance
 LaRelance.opt2 <- function(Y, c1) {
   return(Y > c1)
 }
 
-Karkoli1 <- function(X, )
+# naključna izbira vrednosti pri kateri bo stavil 1.igralec
+Karkoli1 <- function(X, random1) {
+  return(X > random1)
+}
+
+# naključna izbira vrednosti pri kateri bo stavil 2.igralec
+Karkoli2 <- function(Y, random2) {
+  return(Y > random2)
+}
 
 LaRelance <- function(st1, st2, B, n = 100) {
   X <- runif(n, min=0, max=1)
   Y <- runif(n, min=0, max=1)
   c1 <- B/(B+2) #mejna vrednost za stavo 
+  random1 <- runif(n, min=0, max=1)
+  random2 <- runif(n, min=0, max=1)
   R1 <- c() #dobiček/izguba posamezne igre 1.igralca
   R2 <- c() #dobiček/izguba posamezne igre 2.igralca
   Rprvega <- c(0) #Spreminjaje dobička skozi igre 1.igralca
@@ -60,8 +70,6 @@ LaRelance <- function(st1, st2, B, n = 100) {
 }
 
 V1 <- -(B^2)/(B+2)^2 #vrednost igre za 1.igralca 
-
-
 
 sum(R1)
 sum(R2)
